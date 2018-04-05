a24ae1f7df924dce349b9f103ef1780dfaa4f8a9
sojood97 <35595423+sojood97@users.noreply.github.com>
2018-04-05 12:14:10 +0300

    67678

---
+++ src/son/sonar1.java
@@ -0,0 +1,21 @@
+package son;
+
+import java.util.Scanner;
+public class sonar1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		int number1;
+		int number2;
+		int sum;
+		System.out.print("number1");
+		number1=input.nextInt();
+		System.out.print("number2");
+		number2=input.nextInt();
+		sum=number1+number2;
+		System.out.printf("sum %d ",sum);
+
+	}
+
+}
